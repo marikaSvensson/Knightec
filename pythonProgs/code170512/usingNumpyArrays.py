6bce6097dac531ea6530bdef59fc01d223b72f7a
Marika <marika@marika-VirtualBox.(none)>
2017-05-12 13:43:35 +0200

    numpy init

--- pythonProgs/code170512/usingNumpyArrays.py
+++ pythonProgs/code170512/usingNumpyArrays.py
@@ -1,4 +1,13 @@
 import numpy as np
 
 N = np.zeros([10,10])
-print N
+print N, "\n#elements: " ,np.size(N), "dimensions: ",np.shape(N)
+
+
+N = np.ones([10,10])
+print N, "\n#elements: " ,np.size(N), "dimensions: ",np.shape(N)
+
+
+N = np.ones([1,10])
+print N, "\n#elements: " ,np.size(N), "dimensions: ",np.shape(N)
+print N[0][0:9] # simply an array 
